33b74b3e71271c52b15c0039de31c619cda12028
Anna211291 <Anjutka777@gmail.com>
2023-09-06 03:50:26 -0400

    Summary

--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ const form = document.querySelector("#search-form");
 const galleryList = document.querySelector(".gallery");
 const loadBtn = document.querySelector(".load-more");
 
-let page = "1";
+ let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+ });
+
+let page = 1;
 let query = "";
+const per_page = 40;
 const { searchQuery } = form.elements;
 
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "39215228-7f7f32c48d65cadc310432918"
 
-async function getImages() {
+async function getImages(query, page) {
     const params = new URLSearchParams({
     key: `${API_KEY}`,
     q: `${query}`,
@@ -22,62 +28,94 @@ async function getImages() {
     orientation: "horizontal",
     safesearch: "true",
     page: `${page}`,
-    per_page: 40
+    per_page: `${per_page}`
 });
   try {
     const response = await axios.get(`${BASE_URL}?${params}`);
-      // console.log(response);
     return response.data;
   } catch (error) {
     console.error(error);
   }
 }
-console.log(getImages());
-form.addEventListener("submit", searchSubmit);
 
-async function searchSubmit(evt) {
-    evt.preventDefault();
+form.addEventListener("submit", searchBtn);
+loadBtn.addEventListener("click", loadMoreBtn);
+
+async function searchBtn(evt) {
+  evt.preventDefault();
+  galleryList.innerHTML = "";
   query = searchQuery.value.trim();
+  page = 1;
+  if (query === "") {
+    Notiflix.Notify.warning('Please fill out the search field');
+    return;
+  }
 
-  // try {
-  //   const data = await getImages(query) 
-  //   galleryList.insertAdjacentHTML("afterbegin", createMarkup(data));
-  // } catch (error) {
-  //   console.error(error);
-  // }
-    getImages().then(data => {
-    galleryList.insertAdjacentHTML("afterbegin", createMarkup(data.hits));
-     
-    })
-    .catch(err => { console.log(err); }) 
+  try {
+    const data = await getImages(query, page)
+    console.log(data);
+    if (data.hits.length === 0) {
+      loadBtn.classList.replace("load-more-style", "hidden");
+      Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+        Notiflix.Notify.info.remove("We&#39;re sorry, but you&#39;ve reached the end of search results.")
+    }
+    else {
+      createMarkup(data.hits)
+      gallery.refresh();
+Notiflix.Notify.info(`'Hooray! We found ${data.totalHits} images.'`);
+    }
+
+    if (page < data.totalHits/per_page) {
+      loadBtn.classList.replace("hidden", "load-more-style");
+    } else {
+      loadBtn.classList.replace("load-more-style", "hidden");
+       Notiflix.Notify.info("We&#39;re sorry, but you&#39;ve reached the end of search results.")
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+  
+async function loadMoreBtn() {
+  page += 1; 
+   try {
+    const data = await getImages(query, page)
+    console.log(data);
+     createMarkup(data.hits)
+   gallery.refresh();
+      const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 1.5,
+  behavior: "smooth",
+});
+     Notiflix.Notify.info(`'Hooray! We found ${data.totalHits} images.'`);
+  
+    if (page < data.totalHits/per_page) {
+      loadBtn.classList.replace("hidden", "load-more-style");
+    }
+    else {
+      loadBtn.classList.replace("load-more-style", "hidden");
+     Notiflix.Notify.info("We&#39;re sorry, but you&#39;ve reached the end of search results.") 
+    }
+  } catch (error) {
+    console.error(error);
+  }  
 }
 
 function createMarkup(arr) {
-    // console.log(arr);
-  return arr.map(({ webformatURL,
-largeImageURL,
-tags,
-likes,
-views,
-comments,
-downloads
-  }) => `
-    <div class="photo-card">
-//   <a href="${largeImageURL}"><img class="image" src="${webformatURL}" alt="${tags}" loading="lazy" width="300" /></a>
-//   <div class="info">
-//     <p class="info-item">Likes: 
-//       <b>${likes}</b>
-//     </p>
-//     <p class="info-item">Views: 
-//       <b>${views}</b>
-//     </p>
-//     <p class="info-item">Comments: 
-//       <b>${comments}</b>
-//     </p>
-//     <p class="info-item">Downloads:
-//       <b>${downloads}</b>
-//     </p>
-//   </div>
-// </div>`).join("")
-
+ const image = arr.map(image => `
+     <div class="photo-card">
+  <a class="img-link" href="${image.largeImageURL}"><img class="image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="300" /></a>
+  <div class="info">
+ <p class="info-item"><span class="text-span">Likes: </span><b>${image.likes}</b></p>
+ <p class="info-item"><span class="text-span">Views: </span><b>${image.views}</b></p>
+ <p class="info-item"><span class="text-span">Comments: </span><b>${image.comments}</b>
+   </p>
+ <p class="info-item"><span class="text-span">Downloads: </span><b>${image.downloads}</b></p>
+ </div>
+ </div>`).join("")
+  galleryList.insertAdjacentHTML("beforeend", image)
 };
